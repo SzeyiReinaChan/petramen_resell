38ae6746bf8a16f2e00ebba99ee30e709bf424ae
szeyichan <szeyic@gmail.com>
2022-03-13 16:38:10 -0400

    Added Back-end User Authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,27 +1,41 @@
 import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
 
 //modeling the user data
-const userSchema =  mongoose.Schema({
-    name:{
-        type:String,
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
         required: true
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
         required: true,
         unique: true
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
         required: true
     },
-    isAdmin:{
-        type:Boolean,
+    isAdmin: {
+        type: Boolean,
         required: true,
         default: false,
     }
 }, {
     timestamps: true
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")){
+        next()
+    }
+    
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 const User = mongoose.model("User", userSchema)
